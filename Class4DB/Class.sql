7f66d00c8b411a01120fb0caf05a34e79f4db2cd
a18094 <hongyu-zhao@hotmail.com>
2019-11-27 18:36:47 -0500

    3 new tables

---
+++ Class4DB/Class.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Class]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CourseName] VARCHAR(50) NULL, 
+    [RoomNum] VARCHAR(50) NULL, 
+    [Credits] INT NULL, 
+    [Teacher] VARCHAR(50) NULL
+)
